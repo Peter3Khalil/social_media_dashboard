e259c9b215073129de4ad6fadd263ac559f5f38a
Peter Khalil <rikokamil97@gmail.com>
2023-07-14 00:13:53 +0300

    Save Theme in LocalStorage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,19 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 export default function App({ Component, pageProps }: AppProps) {
   const [mode, setMode] = useState("");
   const handleMode = () => {
     setMode(mode => mode === "dark" ? "" : "dark")
   }
+  useEffect(()=>{
+   setMode(localStorage.getItem("theme") || "")
+  },[])
+  useEffect(()=>{
+    localStorage.setItem("theme",mode)
+  },[mode])
+  
   return (
     <>
       <Head>
